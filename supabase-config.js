3db81ab2350a58e2b5dfad499a1d2a52ab38fff6
wael3adel-lang <wael3adel@gmail.com>
2026-09-03 04:32:57 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://akiuzveglknlwcqnczfl.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_6UV-xg2rU6nAWgd9c-BIPQ_if-sdbM3";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
-);
