fb39eb3f81507706a2cbae9648c13c64eeb469da
wael3adel-lang <wael3adel@gmail.com>
2026-09-03 03:54:31 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://akiuzveglknlwcqnczfl.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_6UV-xg2rU6nAWgd9c-BIPQ_if-sdbM3";
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
